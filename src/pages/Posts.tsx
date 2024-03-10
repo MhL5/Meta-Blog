6ed6159bb6fdabe795dd7268ee15e94451321381
MhL5 <sapumr@gmail.com>
2024-03-10 23:44:06 +0330

    add cards to  posts page

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,12 +1,23 @@
 import { ReactElement } from "react";
 import DefaultPageContainer from "../ui/DefaultPageContainer";
 import Header from "../ui/Header";
+import { useGetArticles } from "../hooks/useGetArticles";
+import ArticleCard from "../ui/ArticleCard";
 
 function Posts(): ReactElement {
+  const { articles, error, isLoading } = useGetArticles();
+
+  if (isLoading) return <span>loading...</span>;
+  if (error || !articles) return <span>Error</span>;
   return (
     <DefaultPageContainer>
       <Header />
-      POSTS
+      <h2 className="mt-10 text-center text-7xl font-bold">Posts</h2>
+      <div className="m-auto mt-10 grid max-w-globalWidthContent grid-cols-3 gap-8">
+        {articles.map((card) => (
+          <ArticleCard {...card} key={card.id} />
+        ))}
+      </div>
     </DefaultPageContainer>
   );
 }
