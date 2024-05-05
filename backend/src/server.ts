092771a5084e9c4582dc7a660abb798a0a7ed2dd
MhL5 <sapumr@gmail.com>
2024-05-05 10:36:21 +0330

    backend:add express update dbSchema diagram

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,22 @@
-console.log(`HELLO NODE JS`);
+import express from "express";
+
+const app = express();
+
+app.listen(3000);
+
+let views = 0;
+let viewers: string[] = [];
+
+app.get("/", async (req, res, next) => {
+  console.log(req.ip);
+  if (req.ip) {
+    const exist = viewers.find((ip) => ip === req.ip);
+    console.log(exist);
+    if (!exist) {
+      views++;
+      viewers.push(req.ip);
+    }
+    console.log(views);
+  }
+  res.send("yo");
+});
