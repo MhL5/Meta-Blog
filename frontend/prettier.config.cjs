69169f7416329994124eef6d0efc6cdc5579d0f6
MhL5 <sapumr@gmail.com>
2024-05-11 12:37:19 +0330

    remove frontend code for a fresh start

--- frontend/prettier.config.cjs
+++
@@ -1,5 +0,0 @@
-// prettier.config.js
-// eslint-disable-next-line no-undef
-module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
-};
--- frontend/src/utils/TextTruncate.ts
+++
@@ -1,9 +0,0 @@
-export function textTruncate(text: string, maxLength: number): string {
-  // Guard Clause:
-  if (text.length < maxLength) return text;
-
-  const textSlice = text.slice(0, maxLength).split(" ");
-  textSlice.pop();
-
-  return textSlice.join(" ") + "...";
-}
