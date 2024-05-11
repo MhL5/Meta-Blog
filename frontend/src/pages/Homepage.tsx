a12cb8c14f1c6fea837e644a26802e784051679e
MhL5 <sapumr@gmail.com>
2024-05-11 23:42:51 +0330

    frontend: add router and pages

---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+    return (
+        <>
+            homepage
+        </>
+    )
+}
---
+++ frontend/src/router/Router.tsx
@@ -0,0 +1,30 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import ArticlesPage from "@/pages/ArticlesPage";
+import AuthorsPage from "@/pages/AuthorsPage";
+import ErrorPage from "@/pages/ErrorPage";
+import Homepage from "@/pages/Homepage";
+import NotFoundPage from "@/pages/NotFoundPage";
+import SignUpPage from "@/pages/SignUpPage";
+import TagsPage from "@/pages/TagsPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<ErrorPage />}>
+      <Route path="/" element={<Homepage />} />
+
+      <Route path="authors" element={<AuthorsPage />} />
+      <Route path="articles" element={<ArticlesPage />} />
+      <Route path="tags" element={<TagsPage />} />
+
+      <Route path="signup" element={<SignUpPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
+
+export { router };
