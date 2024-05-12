b7011641de066d6bd1f0dd59dc14c278244d30ff
MhL5 <sapumr@gmail.com>
2024-05-12 19:43:28 +0330

    frontend: update ErrorPage

--- frontend/src/pages/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage.tsx
@@ -1,3 +1,23 @@
-export default function ErrorPage() {
-  return <h1>ERROR</h1>;
+import { Button } from "@/components/ui/button";
+import Header from "../components/layout/Header";
+import { Link } from "react-router-dom";
+import PageContainer from "@/components/layout/PageContainer";
+
+function ErrorPage() {
+  return (
+    <PageContainer>
+      <div className="flex min-h-dvh flex-col">
+        <Header />
+        <div className="m-auto text-center">
+          <div className="my-14 text-7xl">ERROR: SOMETHING WENT WRONG!😥</div>
+          <Button asChild>
+            <Link to="/">Homepage</Link>
+          </Button>
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </PageContainer>
+  );
 }
+
+export default ErrorPage;
