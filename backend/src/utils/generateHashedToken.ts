f0ede4cd6eda9368577570dd47fbe5be61227758
MhL5 <sapumr@gmail.com>
2024-05-21 20:59:47 +0330

    backend: update generatGenerateReadHashedToken docs

--- backend/src/utils/generateHashedToken.ts
+++ backend/src/utils/generateHashedToken.ts
@@ -1,26 +1,25 @@
 import crypto from "crypto";
 
+/**
+ * Utility class for generating and handling hashed tokens using crypto.
+ * @description you can generate, hash, and read the hashed tokens using this class.
+ */
 class GenerateReadHashedToken {
   constructor() {}
 
   /**
    * Generate a token using crypto
-   * @returns
    */
   generate = () => crypto.randomBytes(32).toString("hex");
 
   /**
    * hash your token
-   * @param token
-   * @returns
    */
   toHash = (token: string) =>
     crypto.createHash("sha256").update(token).digest("hex");
 
   /**
    * Read your hashed token
-   * @param token
-   * @returns
    */
   readHash = (token: string) =>
     crypto.createHash("sha256").update(token).digest("hex");
