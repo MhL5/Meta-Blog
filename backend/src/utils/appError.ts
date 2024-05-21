6c2be11227fee2917763d2fd8f3eed169b7836b1
MhL5 <sapumr@gmail.com>
2024-05-21 21:14:54 +0330

    backend: update AppError docs

--- backend/src/utils/appError.ts
+++ backend/src/utils/appError.ts
@@ -1,3 +1,9 @@
+/**
+ * Custom error class for creating errors to be sent to the Express error handler middleware. 
+ * @example 
+ * return next(new AppError("this will be sent to express error handler",statusCode));
+ * return next(new AppError("username or password is not valid",400));
+ */
 class AppError extends Error {
   statusCode: number;
   status: "fail" | "Error";
