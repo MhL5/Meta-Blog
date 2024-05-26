a2730c8299312841a77251e89da0328979a22a43
Mohammad Lashani <sapumr@gmail.com>
2024-05-26 19:09:56 +0330

    backend: update api limiters

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import authController from "../controllers/authControllers/authController";
+import { loginApiLimiter } from "../middlewares/rateLimiter";
 
 const router = express.Router();
 
 router.route("/refresh").get(authController.handleRefreshToken);
 router.route("/signup").post(authController.handleSignUp);
-router.route("/login").post(authController.handleLogin);
+router.route("/login").post(loginApiLimiter, authController.handleLogin);
 router.route("/logout").get(authController.handleLogout);
 router.route("/verify-email/:token").get(authController.handleVerifyEmail);
 
