dfe7b015c06605cefa4ff96d9f11d41ecd5183f8
Mohammad Lashani <sapumr@gmail.com>
2024-05-26 17:03:06 +0330

    backend: update setAuthCookiesAndRespond to remove unwanted fields from response

--- backend/src/controllers/authControllers/utils/authUtils.ts
+++ backend/src/controllers/authControllers/utils/authUtils.ts
@@ -1,12 +1,12 @@
 import jwt from "jsonwebtoken";
 import { Response } from "express";
-import { IUser } from "../../../model/userModel";
+import { IUser, UserDocuments } from "../../../model/userModel";
 import { env } from "../../../utils/env";
 import { promisify } from "util";
 
 type TokenGeneratorParams = {
   res: Response;
-  user: IUser;
+  user: UserDocuments;
 };
 
 type setAuthCookiesAndRespondParams = TokenGeneratorParams & {
@@ -33,50 +33,62 @@ export function isDecodedAccessToken(
 
 export const jwtVerifyAsync = promisify<string, string>(jwt.verify);
 
-/**
- * Class representing utility functions for authentication operations.
- * Includes methods for
- * * generating refresh and access tokens,
- * * setting authentication cookies,
- * * cleaning cookies.
- */
-class AuthUtils {
-  constructor() {}
-
-  generateRefreshToken({ res, user }: TokenGeneratorParams) {
-    return jwt.sign({ email: user.email }, env.REFRESH_TOKEN_SECRET, {
-      expiresIn: "1d",
-    });
-  }
-
-  generateAccessToken({ res, user }: TokenGeneratorParams) {
-    return jwt.sign({ data: { user } }, env.ACCESS_TOKEN_SECRET, {
-      expiresIn: "10s",
-    });
-  }
-
-  /**
-   * Sets  `refresh cookie` and responds with `{ data: { user }, accessToken }`.
-   */
-  setAuthCookiesAndRespond({
-    res,
-    user,
-    newRefreshToken,
-    accessToken,
-  }: setAuthCookiesAndRespondParams) {
-    res.cookie("jwt", newRefreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 24 * 60 * 60 * 1000,
-    });
-
-    res.status(200).json({ data: { user }, accessToken });
-  }
-
-  cookieCleaner({ res }: cookieCleanerParams) {
-    res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
-  }
+function generateRefreshToken({ res, user }: TokenGeneratorParams) {
+  return jwt.sign({ email: user.email }, env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "1d",
+  });
 }
 
-export default new AuthUtils();
+function generateAccessToken({ res, user }: TokenGeneratorParams) {
+  return jwt.sign({ data: { user } }, env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "10s",
+  });
+}
+
+/**
+ * - Sets  `refresh cookie`
+ * - removes unwanted fields
+ * - responds with `{ data: { user }, accessToken }`.
+ */
+function setAuthCookiesAndRespond({
+  res,
+  user,
+  newRefreshToken,
+  accessToken,
+}: setAuthCookiesAndRespondParams) {
+  /**
+   * Setting cookie
+   */
+  res.cookie("jwt", newRefreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
+
+  /**
+   * Removing unwanted fields from user object for Security
+   */
+  const keysToDelete = ["__v", "password", "active", "refreshToken"] as const;
+  const updatedUser = user.toObject();
+  keysToDelete.forEach((key) => delete updatedUser[key]);
+
+  res.status(200).json({ data: { user: updatedUser }, accessToken });
+}
+
+/**
+ * Sending the response
+ */
+function cookieCleaner({ res }: cookieCleanerParams) {
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
+}
+
+// TODO: FIX THIS MESS
+const AuthUtils = {
+  cookieCleaner,
+  setAuthCookiesAndRespond,
+  generateAccessToken,
+  generateRefreshToken,
+};
+
+export default AuthUtils;
