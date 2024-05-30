b4b5f5542815e4ee9cb73b8a7f11bc09cdf2cdcf
Mohammad Lashani <sapumr@gmail.com>
2024-05-30 13:05:16 +0330

    backend: add commentController, update articleRouter to support creating updating and deleting comments

--- backend/src/routes/articleRouter.ts
+++ backend/src/routes/articleRouter.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import * as articleController from "../controllers/articleController";
+import * as commentController from "../controllers/commentController";
+import { protect } from "../middlewares/protect";
 
 const router = express.Router();
 
@@ -13,5 +15,16 @@ router
   .get(articleController.getArticle)
   .delete(articleController.deleteArticle)
   .patch(articleController.updateArticle);
+
+/**
+ * Protecting comment route from un authorized access
+ */
+router.use(protect);
+router.route("/comments").post(commentController.createComment);
+
+router
+  .route("/comments/:id")
+  .patch(commentController.updateComment)
+  .delete(commentController.deleteComment);
 
 export default router;
