82eff89c6a7feb1a201f0c68a73d2945d6b84e97
Mohammad Lashani <sapumr@gmail.com>
2024-05-31 01:20:45 +0330

    fix render mark down styles(not working because of in correct theme imports)

--- frontend/src/features/articles/RenderMarkDown.tsx
+++ frontend/src/features/articles/RenderMarkDown.tsx
@@ -1,5 +1,5 @@
 import ReactMarkdown from "react-markdown";
-import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/prism";
 import { useDarkModeContext } from "../theme/DarkModeContext";
 import {
   oneDark,
@@ -40,3 +40,46 @@ export default function RenderMarkDown({ data }: RenderMarkDownProps) {
     </article>
   );
 }
+
+// import ReactMarkdown from "react-markdown";
+// import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+// import { useDarkModeContext } from "../theme/DarkModeContext";
+// import {
+//   oneDark,
+//   oneLight,
+// } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+// type RenderMarkDownProps = { data: string };
+
+// export default function RenderMarkDown({ data }: RenderMarkDownProps) {
+//   const { isDarkMode } = useDarkModeContext();
+
+//   return (
+//     <article className="prose prose-slate max-w-none dark:prose-invert prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-pre:bg-transparent prose-pre:p-0">
+//       <ReactMarkdown
+//         children={data}
+//         components={{
+//           code(props) {
+//             // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//             const { children, className, node, ...rest } = props;
+//             const match = /language-(\w+)/.exec(className || "");
+//             return match ? (
+//               <SyntaxHighlighter
+//                 {...rest}
+//                 PreTag="div"
+//                 children={String(children).replace(/\n$/, "")}
+//                 language={match[1]}
+//                 style={isDarkMode ? oneDark : oneLight}
+//                 wrapLongLines={true}
+//               />
+//             ) : (
+//               <code {...rest} className={className}>
+//                 {children}
+//               </code>
+//             );
+//           },
+//         }}
+//       />
+//     </article>
+//   );
+// }
