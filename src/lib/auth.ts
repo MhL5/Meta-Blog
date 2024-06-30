30cf298b78cf31cd527df1bd7ad6197170d62de6
Mohammad Lashani <sapumr@gmail.com>
2024-06-30 23:13:14 +0330

    add auth.ts with google github credentials provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,43 +1,79 @@
-import NextAuth, { NextAuthConfig } from "next-auth";
+import NextAuth, { CredentialsSignin, NextAuthConfig } from "next-auth";
 import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prismaClient from "./prismaClient";
+import credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import { ZodError, z } from "zod";
+import bcrypt from "bcrypt";
+import { fromZodError } from "zod-validation-error";
+
+class InvalidLoginError extends CredentialsSignin {
+  code = "Invalid identifier or password";
+}
+
+const signInSchema = z.object({
+  email: z.string().email().min(100),
+  password: z.string().min(100),
+});
 
 const AuthOptions = {
+  adapter: PrismaAdapter(prismaClient),
   providers: [
     Google,
-    // Credentials({
-    //   // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    //   // e.g. domain, username, password, 2FA token, etc.
-    //   name: "password",
-    //   credentials: {
-    //     email: { label: "Email", type: "email", placeholder: "Email" },
-    //     password: {
-    //       label: "Password",
-    //       type: "password",
-    //       placeholder: "Password",
-    //     },
-    //   },
-    //   authorize: async (credentials) => {
-    //     // todo: implement logic to verify credentials
-    //     let user = null;
+    Github,
+    credentials({
+      name: "password",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@email.com",
+          name: "email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "********",
+          name: "password",
+        },
+      },
+      /*
+       * 1. username and password exist in req.body?
+       * 2. user exist and password match?
+       * 3. if everything ok send the data
+       */
+      authorize: async (credentials) => {
+        try {
+          // * 1. validate username and password exist
+          const { email, password } = signInSchema.parse(credentials);
 
-    //     // logic to salt and hash password
-    //     const pwHash = saltAndHashPassword(credentials.password);
+          // * 2. user exist and password match?
+          const user = await prismaClient.user.findUnique({
+            where: { email },
+          });
+          const isCorrectPassword = !(
+            user?.password && !(await bcrypt.compare(password, user?.password))
+          );
+          if (!user || !isCorrectPassword) throw new InvalidLoginError();
 
-    //     // logic to verify if user exists
-    //     user = await getUserFromDb(credentials.email, pwHash);
+          // * 3. if everything ok send the data
+          return user;
+        } catch (error) {
+          // handling zod error with zod-validation-error library
+          if (error instanceof ZodError && fromZodError(error))
+            throw new Error(
+              error.issues.map((i) => `${i.path} ${i.message}`).join(", ")
+            );
 
-    //     if (!user) {
-    //       // No user found, so this is their first attempt to login
-    //       // meaning this is also the place you could do registration
-    //       throw new Error("User not found.");
-    //     }
-
-    //     // return user object with the their profile data
-    //     return user;
-    //   },
-    // }),
+          return null;
+        }
+      },
+    }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, signIn, signOut, auth } = NextAuth(AuthOptions);
