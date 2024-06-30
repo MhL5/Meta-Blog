8ff76cb9d127020b53e2e22188dc9fd37f165df7
Mohammad Lashani <sapumr@gmail.com>
2024-06-30 16:37:38 +0330

    update env variables schema

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,9 +4,15 @@ import { z } from "zod";
  * The Zod schema for validating environment variables.
  */
 const envSchema = z.object({
+  // NEXT-AUTH KEYS:
   AUTH_SECRET: z.string().min(1),
   AUTH_GOOGLE_SECRET: z.string().min(1),
   AUTH_GOOGLE_ID: z.string().min(1),
+  // Mongodb atlas KEY:
+  DATABASE_URL: z.string().min(1),
+  // RECAPTCHA KEYS:
+  NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY: z.string().min(1),
+  GOOGLE_RECAPTCHA_SECRET: z.string().min(1),
 });
 
 /**
