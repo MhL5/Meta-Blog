b1dfcb7485022077f5155fb5432f46280afa611d
Mohammad Lashani <sapumr@gmail.com>
2024-07-06 18:45:52 +0330

    add UUIDGenerator

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Generates a random UUID (Universally Unique Identifier).
+ * @returns {string} A randomly generated UUID string.
+ * ### Only works in server components ( node crypto module does not work in browser )
+ */
+export function UUIDGenerator(): string {
+  return crypto.randomUUID();
+}
