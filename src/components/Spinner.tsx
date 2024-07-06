f1381a30161385204364310db53215887b8cda0e
Mohammad Lashani <sapumr@gmail.com>
2024-07-06 15:51:41 +0330

    add size prop to Spinner component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,3 +1,8 @@
-export default function Spinner() {
-  return <div className="spinner" />;
+import { LoaderCircle } from "lucide-react";
+
+export default function Spinner({ size }: { size: "sm" | "md" }) {
+  let spinnerClass = "spinner";
+  if (size === "sm") spinnerClass = "spinner-mini";
+
+  return <LoaderCircle className={spinnerClass} />;
 }
