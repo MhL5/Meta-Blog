208f6c24e9536b6b686f7a95b8ba1def34a0aa92
Mohammad Lashani <sapumr@gmail.com>
2024-07-07 22:06:28 +0330

    change style

--- src/features/newsLetterSubscription/NewsLetterSubscription.tsx
+++ src/features/newsLetterSubscription/NewsLetterSubscription.tsx
@@ -39,7 +39,7 @@ export default function NewsLetterSubscription() {
   }
 
   return (
-    <div className="h-[20rem] w-full rounded-md bg-muted/50 backdrop-blur-md shadow-xl border-[0.1px] relative flex flex-col items-center justify-center antialiased">
+    <div className="h-[20rem] my-16 w-full rounded-md bg-muted/50 shadow-xl border-[0.1px] relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-5xl md:mb-6  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold">
           Meta Blog newsletter
