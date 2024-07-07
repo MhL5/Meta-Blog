b2024642980718358327fb06e2948f31001c8fad
Mohammad Lashani <sapumr@gmail.com>
2024-07-07 17:02:59 +0330

    add md size to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,6 +21,7 @@ const buttonVariants = cva(
       },
       size: {
         default: "h-10 px-4 py-2",
+        md: "h-10 rounded-md px-3",
         sm: "h-9 rounded-md px-3",
         xs: "h-8 rounded-md px-3 text-xs",
         lg: "h-11 rounded-md px-8",
