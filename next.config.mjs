50a9d1e90efe320a8af7630d08905dd8d8931432
Mohammad Lashani <sapumr@gmail.com>
2024-07-08 00:06:19 +0330

    add cloudinary github to images object

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "lh3.googleusercontent.com" }],
+    remotePatterns: [
+      { hostname: "lh3.googleusercontent.com" },
+      { hostname: "raw.githubusercontent.com" },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dkyoa6any",
+      },
+    ],
   },
 };
 
