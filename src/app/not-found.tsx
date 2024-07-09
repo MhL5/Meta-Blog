9c030a4a20011da1b1afbc90f8ffa9b0a0adf2c5
Mohammad Lashani <sapumr@gmail.com>
2024-07-10 01:15:56 +0330

    update not found page height

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,19 +3,21 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="space-y-6 text-center m-8">
-      <h1 className="text-3xl font-semibold">
-        This page could not be found ☹️
-      </h1>
+    <div className="min-h-[60dvh] grid place-items-center text-center">
+      <main className="space-y-6">
+        <h1 className="text-3xl font-semibold">
+          This page could not be found ☹️
+        </h1>
 
-      <Button asChild>
-        <Link
-          href="/"
-          className="bg-accent-500 text-primary-800 btn btn-primary inline-block px-6 py-3 text-lg"
-        >
-          Go back home
-        </Link>
-      </Button>
+        <Button asChild>
+          <Link
+            href="/"
+            className="bg-accent-500 text-primary-800 btn btn-primary inline-block px-6 py-3 text-lg"
+          >
+            Go back home
+          </Link>
+        </Button>
+      </main>
     </div>
   );
 }
