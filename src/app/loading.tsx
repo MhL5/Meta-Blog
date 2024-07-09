1026a5e52da172c64db5ad2e58c2537ea700bbf0
Mohammad Lashani <sapumr@gmail.com>
2024-07-10 01:16:24 +0330

    update app loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,9 @@
-import Spinner from "@/components/Spinner";
+import Logo from "@/components/Logo";
 
 export default function loading() {
-  return <Spinner />;
+  return (
+    <div className="min-h-[80dvh] grid place-items-center">
+      <Logo className="animate-ping duration-1000 scale-150" />
+    </div>
+  );
 }
