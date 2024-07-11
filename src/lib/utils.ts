bc5a5055bbcb6488a8bf6e96c53a3acc21553f4f
Mohammad Lashani <sapumr@gmail.com>
2024-07-12 00:33:41 +0330

    add truncateText

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,3 +13,10 @@ export function cn(...inputs: ClassValue[]) {
 export function UUIDGenerator(): string {
   return crypto.randomUUID();
 }
+
+/**
+ * Truncates the given text string to the specified limit, appending '...' if it exceeds the limit.
+ */
+export function truncateText(str: string, limit = 100) {
+  return str.length > limit ? str.slice(0, limit) + "..." : str;
+}
