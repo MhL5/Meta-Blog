db68ecfbdc3f5672383235446b6593cd140937be
Mohammad Lashani <sapumr@gmail.com>
2024-07-13 23:00:05 +0330

    TODO ADD

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,5 @@
-function Search() {
-  return <search>🔍</search>;
+export default function Search2() {
+  return (
+    <div className="relative ml-auto flex-1 md:grow-0">`TODO:COMMAND`</div>
+  );
 }
-
-export default Search;
