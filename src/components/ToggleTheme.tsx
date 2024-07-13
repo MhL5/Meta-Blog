4bbaecec815b09b40d959cdb1e6d9e21d48b5487
Mohammad Lashani <sapumr@gmail.com>
2024-07-13 10:26:15 +0330

    change the loader for ToggleTheme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren, ReactElement, useEffect, useState } from "react";
 import { Button, ButtonProps } from "@/components/ui/button";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
-import Image from "next/image";
+import { Skeleton } from "./ui/skeletion";
 
 type ToggleThemeProps = PropsWithChildren & ButtonProps;
 
@@ -18,18 +18,7 @@ function ToggleTheme({
 
   useEffect(() => setMounted(true), []);
 
-  if (!mounted)
-    return (
-      <Image
-        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-        width={30}
-        height={30}
-        sizes="30x30"
-        alt="Loading Light/Dark Toggle"
-        priority={false}
-        title="Loading Light/Dark Toggle"
-      />
-    );
+  if (!mounted) return <Skeleton className="h-6 w-6" />;
 
   return (
     <Button
