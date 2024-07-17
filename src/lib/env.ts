40c40531831baa426233483dfff364a502199eb1
Mohammad Lashani <sapumr@gmail.com>
2024-07-17 19:43:39 +0330

    improve .env schema

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,7 +2,11 @@ import { z } from "zod";
 
 /**
  * The Zod schema for validating environment variables.
+ * Public environment are not useable in client components and results in zod Error we only write them here as a reference
+ * we have to use process.env for public environments
  */
+const publicEnvErrorMessage =
+  "next public environment variables are not accessible in client components WITH ZOD schema, only in server components,use process.env instead";
 const envSchema = z.object({
   // NEXT-AUTH KEYS:
   AUTH_SECRET: z.string().min(1),
@@ -15,13 +19,31 @@ const envSchema = z.object({
   // Mongodb atlas KEY:
   DATABASE_URL: z.string().min(1),
   // RECAPTCHA KEYS:
-  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z.string().min(1),
+  NEXT_PUBLIC_RECAPTCHA_SITE_KEY: z
+    .string({
+      message: publicEnvErrorMessage,
+    })
+    .min(1),
   GOOGLE_RECAPTCHA_SECRET: z.string().min(1),
   // cloudinary
-  NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z.string().min(1),
+  NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: z
+    .string({
+      message: publicEnvErrorMessage,
+    })
+    .min(1),
   CLOUDINARY_URL: z.string().min(1),
-  NEXT_PUBLIC_CLOUDINARY_API_KEY: z.string().min(1),
+  NEXT_PUBLIC_CLOUDINARY_API_KEY: z
+    .string({
+      message: publicEnvErrorMessage,
+    })
+    .min(1),
   CLOUDINARY_API_SECRET: z.string().min(1),
+  // application domain
+  NEXT_PUBLIC_APPLICATION_DOMAIN: z
+    .string({
+      message: publicEnvErrorMessage,
+    })
+    .min(1),
 });
 
 /**
