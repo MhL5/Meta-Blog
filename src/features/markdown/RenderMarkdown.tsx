745d4c931639cf9f72e674893a07bfb55605a440
Mohammad Lashani <sapumr@gmail.com>
2024-07-18 10:35:41 +0330

    update render markdown add dom purify to prevent xss attacks

--- src/features/markdown/RenderMarkdown.tsx
+++ src/features/markdown/RenderMarkdown.tsx
@@ -3,6 +3,8 @@ import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import createDOMPurify from "dompurify";
+import { JSDOM } from "jsdom";
 
 type RenderMarkdownProps = { markdown: string };
 
@@ -10,34 +12,42 @@ type RenderMarkdownProps = { markdown: string };
  * receives a markdown string and renders it as HTML, including syntax highlighting for code blocks.
  */
 export default function RenderMarkdown({ markdown }: RenderMarkdownProps) {
-  return (
-    <Markdown
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
-      className="prose prose-slate max-w-none dark:prose-invert prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-pre:bg-transparent prose-pre:p-0"
-      components={{
-        code({ node, inline, className, children, ...props }: any) {
-          const match = /language-(\w+)/.exec(className || "");
+  // sanitize markdown
+  // this happens on server so we need JSDOM to create a window object for DOMPurify.
+  const window = new JSDOM("").window;
+  const DOMPurify = createDOMPurify(window);
+  const sanitizeMarkdown = DOMPurify.sanitize(markdown);
 
-          return !inline && match ? (
-            <SyntaxHighlighter
-              style={oneDark}
-              PreTag="div"
-              language={match[1]}
-              showLineNumbers={true}
-              {...props}
-            >
-              {String(children).replace(/\n$/, "")}
-            </SyntaxHighlighter>
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    >
-      {markdown}
-    </Markdown>
+  return (
+    <article className="prose prose-slate max-w-none dark:prose-invert prose-a:text-blue-600 hover:prose-a:text-blue-500 prose-pre:bg-transparent prose-pre:p-0">
+      <Markdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        disallowedElements={["script"]}
+        components={{
+          code({ node, inline, className, children, ...props }: any) {
+            const match = /language-(\w+)/.exec(className || "");
+
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={oneDark}
+                PreTag="div"
+                language={match[1]}
+                showLineNumbers={true}
+                {...props}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {sanitizeMarkdown}
+      </Markdown>
+    </article>
   );
 }
