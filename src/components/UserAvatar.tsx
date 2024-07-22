ab2aaf00b9e9a95221a688edc0649982d1342223
Mohammad Lashani <sapumr@gmail.com>
2024-07-23 00:26:37 +0330

    remove useMemo from UserAvatar

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,5 +1,4 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { memo, useMemo } from "react";
 
 type UserAvatarProps = {
   imageSrc: string;
@@ -11,26 +10,23 @@ type UserAvatarProps = {
  * a component that renders a user profile or avatar
  * sometimes google images does not show up which we can fix by adding `referrerPolicy="no-referrer"` to image
  */
-const UserAvatar = memo(
-  ({ className, imageSrc, username }: UserAvatarProps) => {
-    const randomBg = useMemo(() => randomBgColor(username), [username]);
+const UserAvatar = ({ className, imageSrc, username }: UserAvatarProps) => {
+  const randomBg = randomBgColor(username);
 
-    return (
-      <Avatar className={`${className} text-slate-800 dark:text-slate-300`}>
-        <AvatarImage
-          src={imageSrc}
-          loading="lazy"
-          referrerPolicy="no-referrer"
-          alt="logged in user profile picture"
-        />
-        <AvatarFallback className={`${randomBg}`}>
-          {username.slice(0, 2).toUpperCase()}
-        </AvatarFallback>
-      </Avatar>
-    );
-  },
-);
-UserAvatar.displayName = `UserAvatar`;
+  return (
+    <Avatar className={`${className} text-slate-800 dark:text-slate-300`}>
+      <AvatarImage
+        src={imageSrc}
+        loading="lazy"
+        referrerPolicy="no-referrer"
+        alt="logged in user profile picture"
+      />
+      <AvatarFallback className={`${randomBg}`}>
+        {username.slice(0, 2).toUpperCase()}
+      </AvatarFallback>
+    </Avatar>
+  );
+};
 
 function randomBgColor(username: string) {
   const randomBgColor = [
