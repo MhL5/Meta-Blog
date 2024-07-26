9710a257c6da126b026e2fcecaf1b41c953bbeb0
Mohammad Lashani <sapumr@gmail.com>
2024-07-26 15:19:52 +0330

    fix optimistic updates merging them into one state inside articleContext for better performance and cleaner code

--- src/app/article/[articleSlug]/CommentsList.tsx
+++ src/app/article/[articleSlug]/CommentsList.tsx
@@ -1,90 +1,16 @@
 "use client";
 
 import { MessageCircleMore } from "lucide-react";
-import { useOptimisticAction } from "next-safe-action/hooks";
 import Link from "next/link";
 import AddComment from "./AddComment";
 import { useArticleContext } from "./ArticleContext";
 import Comment from "./Comment";
-import { createComment, deleteComment, updateComment } from "./actions";
-import { useState } from "react";
 
 export default function CommentsList() {
   const {
-    article: { articleComments, slug },
     loggedInUserSession,
+    articleCommentsOptimistic: { optimisticComments },
   } = useArticleContext();
-  const [ops, setOps] = useState("");
-
-  const {
-    execute: createCommentAction,
-    optimisticState: createCommentOps,
-    isExecuting: isCreatingComment,
-  } = useOptimisticAction(createComment, {
-    currentState: articleComments,
-    updateFn(state, input) {
-      const { articleSlug, ...inputs } = input;
-
-      if (!loggedInUserSession) return state;
-      return [
-        ...state,
-        {
-          ...inputs,
-          id: `${input.articleId + input.articleSlug}`,
-          userId: loggedInUserSession.id,
-          user: { ...loggedInUserSession },
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ];
-    },
-    onExecute() {
-      setOps("createCommentOps");
-    },
-  });
-
-  const {
-    execute: deleteCommentAction,
-    optimisticState: deleteCommentOps,
-    isExecuting: isDeletingComment,
-  } = useOptimisticAction(deleteComment, {
-    currentState: articleComments,
-    updateFn(state, input) {
-      return state.filter((comment) => comment.id !== input.commentId);
-    },
-    onExecute() {
-      setOps("deleteCommentOps");
-    },
-  });
-
-  const {
-    execute: updateCommentAction,
-    optimisticState: updateCommentOps,
-    isExecuting: isUpdatingComment,
-  } = useOptimisticAction(updateComment, {
-    currentState: articleComments,
-    updateFn(state, input) {
-      return state.map((comment) => {
-        if (comment.id === input.commentId)
-          return {
-            ...comment,
-            content: input.content,
-            updatedAt: new Date(),
-          };
-        return comment;
-      });
-    },
-    onExecute() {
-      setOps("updateCommentOps");
-    },
-  });
-
-  const isWorking = isUpdatingComment || isDeletingComment || isCreatingComment;
-
-  let cmdList = articleComments;
-  if (ops === "deleteCommentOps") cmdList = deleteCommentOps;
-  if (ops === "createCommentOps") cmdList = createCommentOps;
-  if (ops === "updateCommentOps") cmdList = updateCommentOps;
 
   return (
     <section
@@ -97,19 +23,12 @@ export default function CommentsList() {
       </div>
 
       <ul className="grid gap-4">
-        {cmdList.length > 0 ? (
+        {optimisticComments.length > 0 ? (
           <>
-            {cmdList.map((comment) => {
+            {optimisticComments.map((comment) => {
               return (
                 <li key={comment.id}>
-                  <Comment
-                    deleteCommentAction={deleteCommentAction}
-                    updateCommentAction={updateCommentAction}
-                    comment={comment}
-                    loggedInUserId={loggedInUserSession?.id || ""}
-                    articleSlug={slug}
-                    isWorking={isWorking}
-                  />
+                  <Comment comment={comment} />
                 </li>
               );
             })}
@@ -121,7 +40,7 @@ export default function CommentsList() {
         )}
 
         {loggedInUserSession?.id ? (
-          <AddComment createCommentAction={createCommentAction} />
+          <AddComment />
         ) : (
           <li className="my-16 text-center">
             <Link
