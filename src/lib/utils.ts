125c609642c4fc951abde219c0d06c2f85c9d7a0
Mohammad Lashani <sapumr@gmail.com>
2024-07-28 17:03:23 +0330

    organize imports

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-import { boolean, z } from "zod";
 import crypto from "crypto";
+import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
