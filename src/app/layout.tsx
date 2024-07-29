d195466638b2ef9ff42c2bbe7fffb6a5caf62da4
Mohammad Lashani <sapumr@gmail.com>
2024-07-29 20:48:02 +0330

    add thumbnail and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import { type ReactNode } from "react";
-import { Inter as FontSans } from "next/font/google";
-import { cn } from "@/lib/utils";
-import { Toaster } from "@/components/ui/toaster";
-import SvgBackground from "@/components/SvgBackground";
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
+import SvgBackground from "@/components/SvgBackground";
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
 import Providers from "@/Providers/Providers";
 import "@/styles/globals.css";
-import Footer from "@/components/layout/Footer";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import { type ReactNode } from "react";
 
 type layoutProps = Readonly<{
   children: ReactNode;
@@ -16,9 +16,54 @@ type layoutProps = Readonly<{
 const fontSans = FontSans({ subsets: ["latin-ext"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(String(process.env.NEXT_PUBLIC_APPLICATION_DOMAIN)),
   title: "Meta Blog",
   description:
     "Meta blog is a blog application full of articles about various topics, completely free.",
+  keywords: [
+    "Meta Blog",
+    "blog",
+    "articles",
+    "free articles",
+    "code",
+    "javascript",
+    "react",
+    "git",
+    "github",
+    "frontend",
+    "coding blog",
+    "coding articles",
+  ],
+  authors: [
+    { name: "Mohammad Hosein Lashani", url: "https://github.com/MhL5" },
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-snippet": -1,
+    },
+  },
+  openGraph: {
+    title: "Meta Blog",
+    description:
+      "Meta blog is a blog application full of articles about various topics, completely free.",
+    url: String(process.env.NEXT_PUBLIC_APPLICATION_DOMAIN),
+    type: "website",
+    images: [
+      {
+        url: "/meta-blog-thumbnail.png",
+        width: 800,
+        height: 600,
+        alt: "Meta Blog Thumbnail",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }: layoutProps) {
