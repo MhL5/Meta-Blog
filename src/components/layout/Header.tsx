1e9a8586a287133a2b62de6b57acf547cf1ffe25
Mohammad Lashani <sapumr@gmail.com>
2024-08-02 14:51:58 +0330

    fix header bug

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
-import Logo from "@/components/Logo";
 import ArticleSearch from "@/components/ArticleSearch";
+import Logo from "@/components/Logo";
 import { Button, ButtonProps } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -8,6 +8,13 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
 import UserDropDown from "@/components/UserDropDown";
 import { cachedAuth } from "@/lib/auth";
 import { EllipsisVertical } from "lucide-react";
@@ -15,30 +22,30 @@ import Link from "next/link";
 import ToggleTheme from "../ToggleTheme";
 import GradientUnderlineText from "../ui/GradientUnderlineText";
 
-function Header() {
+export default function Header() {
   return (
     <header className="sticky top-0 z-50 bg-background/50 backdrop-blur-md">
-      <NavigationMenu />
+      <NavigationMenuWrapper />
     </header>
   );
 }
 
-const navigationLinks = [
-  {
-    id: "a0b26ca6cafa1b93517d73df60cd409820b85c23093dccec4f8f",
-    href: "/authors",
-    label: "Authors",
-  },
-  {
-    id: "21564eb6f9dcb34a0c0382b0dd8e6eec6cd2b758abd4d61a2d31",
-    href: "/topics",
-    label: "Topics",
-  },
-];
-
-async function NavigationMenu() {
+async function NavigationMenuWrapper() {
   const session = await cachedAuth();
   const user = session?.user;
+
+  const navigationLinks = [
+    {
+      id: "a0b26ca6cafa1b93517d73df60cd409820b85c23093dccec4f8f",
+      href: "/authors",
+      label: "Authors",
+    },
+    {
+      id: "21564eb6f9dcb34a0c0382b0dd8e6eec6cd2b758abd4d61a2d31",
+      href: "/feed",
+      label: "Feed",
+    },
+  ];
 
   return (
     <nav className="m-auto max-w-7xl px-2 py-3 sm:p-4">
@@ -105,6 +112,7 @@ async function NavigationMenu() {
               </Link>
             );
           })}
+          <SubMenu />
         </li>
 
         <li className="mx-auto sm:hidden">
@@ -130,6 +138,106 @@ async function NavigationMenu() {
         </li>
       </ul>
     </nav>
+  );
+}
+
+function SubMenu() {
+  const categories = [
+    {
+      id: "42749a8c793e912a0014ea8c15b93549c6d24b56bbbb1aaf9cc0",
+      category: `web_development`,
+    },
+    {
+      id: "425407a35c29ec3244dd8d9192272a756207382380d457a1c627",
+      category: `devOps`,
+    },
+    {
+      id: "282702bec15fe902996a7918cb97ce4568aa46eebd5664f23d0e",
+      category: `machine_learning`,
+    },
+    {
+      id: "786e35ca4be0d9ab087945b628ab5436d0471d886c57c70ecf36",
+      category: `data_science`,
+    },
+    {
+      id: "e76335cf0b93825966e4d834723664785ac6132d0c6440861d05",
+      category: `cyber_security`,
+    },
+    {
+      id: "8c21456154afa7930ce7cb0aa32588dfad3d2161fdf87f93ad66",
+      category: `ui_ux`,
+    },
+    {
+      id: "f87bd2fab02911b2411c8357972ae678d035961bffadd9dcf889",
+      category: `mobile_development`,
+    },
+    {
+      id: "9f2a8d7d88de67c7bd9b10b11a03152031cdaa6119356f9ac301",
+      category: `game_development`,
+    },
+    {
+      id: "d560e47f4c36bbe53519fa8b2d56a5f27ace223fbafd85a46eee",
+      category: `artificial_intelligence`,
+    },
+    {
+      id: "d2d36148a861caef0e996548829c48da8781456c1f2b0a1274ec",
+      category: `database_management`,
+    },
+    {
+      id: "318c0592a84d320bc5f84b1acf2ba87797c851eaaa3952457dea",
+      category: `version_control`,
+    },
+    {
+      id: "838c45d09698ff1c749c6e9831b5f2f08dc8a7e3d7c8f638ea41",
+      category: `testing_and_qa`,
+    },
+    {
+      id: "38e0946e02b684da33297676d723e9781f99bd8bb6c37f339be5",
+      category: `performance_optimization`,
+    },
+    {
+      id: "3404248ae4b5b618de621db1670422b867cdf3afa01776c0d831",
+      category: `algorithms`,
+    },
+  ];
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <GradientUnderlineText>
+            <NavigationMenuTrigger className="bg-transparent">
+              Categories
+            </NavigationMenuTrigger>
+          </GradientUnderlineText>
+          <NavigationMenuContent>
+            <ul className="h-96 divide-y-2 overflow-y-scroll">
+              {categories.map(({ category, id }) => {
+                return (
+                  <li key={id} className="w-full">
+                    <Button
+                      variant="ghost"
+                      size="sm"
+                      className="w-full justify-start rounded-none"
+                      asChild
+                    >
+                      <Link
+                        href={`/categories?category=${category}`}
+                        className="text-start capitalize"
+                      >
+                        <GradientUnderlineText>
+                          {category.replaceAll("_", " ")}
+                        </GradientUnderlineText>
+                      </Link>
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 }
 
@@ -163,5 +271,3 @@ function HeaderLoginButton({
     </Button>
   );
 }
-
-export default Header;
