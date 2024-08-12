5fe00f7386ae665e7037947cd8eee4a49b77cf7b
Mohammad Lashani <sapumr@gmail.com>
2024-08-12 17:19:07 +0330

    fix imports

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -6,15 +6,15 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import RenderMarkdown from "@/features/react-md-editor/RenderMarkdown";
-import { truncateText } from "@/lib/utils";
+import { truncateText } from "@/utils/truncateText";
 import { Prisma } from "@prisma/client";
 import { Bookmark, Clock4, Heart, MessageCircle } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useMemo } from "react";
 import { Badge } from "./ui/badge";
-import GradientUnderlineText from "./ui/GradientUnderlineText";
 import CategoryBadge from "./ui/categoryBadge";
+import GradientUnderlineText from "./ui/GradientUnderlineText";
 import { Skeleton } from "./ui/skeletion";
 
 type ArticleCardProps = {
@@ -72,7 +72,9 @@ function ArticleCard({ article }: ArticleCardProps) {
 
         <CardContent className="px-4">
           <div className="mb-6 mt-2 space-x-2">
-            <CategoryBadge variant={article.category}>{article.category}</CategoryBadge>
+            <CategoryBadge variant={article.category}>
+              {article.category}
+            </CategoryBadge>
             {[...article.tags.slice(0, 2), "More..."].map((tag, i) => (
               <Badge key={i} variant="secondary">
                 {tag}
