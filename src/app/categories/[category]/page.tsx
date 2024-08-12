337f6c8361e4937eb95219045270d05c19a21aae
Mohammad Lashani <sapumr@gmail.com>
2024-08-12 16:18:12 +0330

    update categories page

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,32 +1,23 @@
-import MetaBlogCategories from "@/constants/MetaBlogCategories";
-import CategoriesListBox from "../CategoriesListBox";
+import ArticleCard from "@/components/ArticleCard";
+import ArticleGridWrapper from "@/components/ArticleGridWrapper";
+import MetaBlogCategories, {
+  categoryBgColor,
+} from "@/constants/MetaBlogCategories";
 import { BadgeAlert } from "lucide-react";
 import { notFound } from "next/navigation";
-import CategoryCards from "./CategoryCards";
-import { Suspense } from "react";
-import ArticleCard from "@/components/ArticleCard";
+import CategoriesListBox from "../CategoriesListBox";
+import { getCategoryArticles } from "./services";
+import { SearchParams } from "@/@types/customType";
 
 type PageProps = {
   params: { category: string };
+  searchParams: SearchParams;
 };
 
-const categoryColor = {
-  web_development: "bg-blue-200/50 dark:bg-blue-900/50",
-  devOps: "bg-red-200/50  dark:bg-red-900/50",
-  machine_learning: "bg-yellow-200/50 dark:bg-yellow-900/50",
-  data_science: "bg-slate-200/50  dark:bg-slate-900/50",
-  cyber_security: " bg-green-200/50  dark:bg-green-900/50 ",
-  ui_ux: "bg-pink-200/50 dark:bg-pink-900/50",
-  mobile_development: "bg-purple-200/50  dark:bg-purple-900/50",
-  game_development: "bg-orange-200/50  dark:bg-orange-900/50",
-  artificial_intelligence: "bg-teal-200/50 dark:bg-teal-900/50",
-  database_management: "bg-indigo-200/50  dark:bg-indigo-900/50",
-  version_control: "bg-cyan-200/50  dark:bg-cyan-900/50",
-  testing_and_qa: "bg-lime-200/50 dark:bg-lime-900/50",
-  algorithms: "bg-stone-200/50 dark:bg-stone-900/50",
-};
-
-export default function Page({ params: { category } }: PageProps) {
+export default function Page({
+  params: { category },
+  searchParams,
+}: PageProps) {
   const validCategory = MetaBlogCategories?.find(
     ({ category: ctg }) => ctg === category,
   );
@@ -37,7 +28,7 @@ export default function Page({ params: { category } }: PageProps) {
     <>
       <section className="my-24 flex items-center justify-center">
         <div
-          className={`${categoryColor[validCategory.category]} scale-150 rounded-lg px-12 py-4 text-3xl capitalize`}
+          className={`${categoryBgColor[validCategory.category]} scale-150 rounded-lg px-12 py-4 text-3xl capitalize`}
         >
           <CategoriesListBox
             category={validCategory.category}
@@ -46,11 +37,12 @@ export default function Page({ params: { category } }: PageProps) {
         </div>
       </section>
 
-      <section className="grid max-w-7xl items-stretch gap-4 p-2 sm:grid-cols-2 md:grid-cols-3">
-        <Suspense fallback={<ArticleCard.Skeleton numSkeletons={9} />}>
-          <CategoryCards category={validCategory.category} />
-        </Suspense>
-      </section>
+      <ArticleGridWrapper
+        mode="async"
+        data={async () => await getCategoryArticles({ category, searchParams })}
+        render={(article) => <ArticleCard key={article.id} article={article} />}
+        suspenseKey={`${category}`}
+      />
     </>
   );
 }
